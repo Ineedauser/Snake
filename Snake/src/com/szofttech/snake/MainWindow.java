1f9dd0a992fe48200284b3d7caf5ecaa4905927a
Csordás Róbert <xdever@gmail.com>
2013-03-02 20:01:18 +0100

    NEW: Bejelentkezési képernyő. Egyelőre nem csinál semmit, csak elindul.

--- Snake/src/com/szofttech/snake/MainWindow.java
+++ Snake/src/com/szofttech/snake/MainWindow.java
@@ -1,8 +1,12 @@
 package com.szofttech.snake;
 
+import com.larvalabs.svgandroid.SVG;
+import com.larvalabs.svgandroid.SVGParser;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ImageView;
 
 public class MainWindow extends Activity {
 
@@ -10,6 +14,10 @@ public class MainWindow extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_window);
+        
+        ImageView image=(ImageView)findViewById(R.id.startImage);
+        SVG svg = SVGParser.getSVGFromResource(getResources(), R.raw.snake);
+        image.setImageDrawable(svg.createPictureDrawable());
     }
  
     @Override
